cf715e12b978662a039d8ba7775e6ee32a5883c8
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-29 12:45:04 -1000

    made class for parking lot objects

---
+++ data/database.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+
+let parkingList = [];
+
+class parkingSpot{
+  constructor(username, address, price, picture, lat, long, rating){
+    this.username = username;
+    this.address = address;
+    this.price = price;
+    this.picture = picture;
+    this.lat = lat;
+    this.long = long;
+  }
+}
+
+module.exports = parkingList;
