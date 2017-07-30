04f4359d3b54cedf1a492104a87af913bcb06089
Reyn-L <Reyn.Leong@gmail.com>
2017-07-29 20:37:20 -1000

    timer

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6*/
+const time = document.getElementById('time');
+let total = 0;
+
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++total;
+  let hour = parseInt(total/3600);
+  let minute = parseInt(total/60);
+  if(hour < 10){hour = "0" + hour;}
+  if(minute < 10){minute = "0" + minute;}
+  time.innerHTML = hour + ':' + minute;
+}
