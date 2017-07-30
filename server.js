327916d886cf3ae05be89df0540277f7314d11ed
kwokbk <kwokbk@hawaii.edu>
2017-07-29 14:02:18 -1000

    map current pos, and marker

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.set('view engine', 'hbs');
 const routes = require('./routes/');
 app.use('/', routes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log(`listening on PORT: ${PORT}`);
 });
